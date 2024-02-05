3f5efb737095aa824c2150b06920ced34dfa8451
ajc3xc <ajc3xc@umsystem.edu>
2024-02-05 05:45:42 -0600

    created rudimentary function to seperate the plants into different numpy arrays

--- workflow/run_script/run_ipynb.sh
+++ workflow/run_script/run_ipynb.sh
@@ -1,5 +1,7 @@
 # ACTIVATE ANACONDA
 eval "$(conda shell.bash hook)"
-conda activate plantenv_pcv4_basic
+conda activate plantenv_pcv4_jupyter
 
-jupyter lab --no-browser --port=9999
+#jupyter lab --no-browser --allow-root
+srun --time=1-00:00:00 -p general --job-name=jupyterlab --pty jupyter lab --no-browser --port=8888
+#jupyter lab --no-browser --port=9999
