3bb6b2abb398c23c5517556e135fed5ba0e3eee8
ajc3xc <ajc3xc@umsystem.edu>
2024-02-22 01:31:14 -0600

    testing out changing jupyter lab setup script so I can see jobid automatically

--- workflow/automated_pipeline_test/automated_pipeline_test.sh
+++ workflow/automated_pipeline_test/automated_pipeline_test.sh
@@ -4,4 +4,6 @@
 eval "$(conda shell.bash hook)"
 conda activate plantenv_pcv4_jupyter
 
-srun --time=1-00:00:00 -p general --job-name=jupyterlab --pty jupyter lab --no-browser --port=8888
+srun --time=1-00:00:00 -p general --job-name=jupyterlab jupyter lab --no-browser --port=8888 &
+echo "$(squeue -u $(whoami))"
+wait
