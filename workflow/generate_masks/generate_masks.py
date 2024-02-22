593c02ed0d28a790d70df673b7f869e6f06b4e9a
ajc3xc <ajc3xc@umsystem.edu>
2024-02-21 20:25:19 -0600

    got script to generate mask files working

--- workflow/generate_masks/generate_masks.py
+++ workflow/generate_masks/generate_masks.py
@@ -3,14 +3,47 @@ import pandas as pd
 from PIL import Image
 from parallel_pandas import ParallelPandas
 
-ParallelPandas.initialize(disable_pr_bar = True)
+import os, functools, shutil, sys
 
-def generate_mask(input_image_path: str, output_mask_path: str):
-    img, _, img_name = pcv.readimage(filename=input_image_path)
-    print(img_name)
+ParallelPandas.initialize(n_cpu=64, disable_pr_bar = True)
+
+def generate_mask(input_image_path: str, output_mask_folder: str=None):
+    try:
+        img0, _, img_name = pcv.readimage(filename=input_image_path)
+    except:
+        return
+    a = pcv.rgb2gray_lab(rgb_img=img0, channel='a')
+    thresholded_image = pcv.threshold.otsu(gray_img=a, object_type='dark')
+    a_fill = pcv.fill(bin_img=thresholded_image, size=50)
+    a_fill = pcv.fill_holes(a_fill)
     
+    a_fill_bw = a_fill.astype(bool).astype('uint8') * 255
+    p = Image.fromarray(a_fill_bw)
+    mask_output_path = os.path.join(output_mask_folder, img_name)
+    #print(mask_output_path)
+    p.save(mask_output_path)
+    return 
 
-input_image_path = "/mnt/stor/ceph/csb/marsfarm/projects/marsfarm_image_analysis/inputs/MV1-0043_ 6.1.23 - 6.16.23 (Purple Basil)/2023-05-31_2305.jpg"
+#get input paths
+input_image_folder = sys.argv[1] #quick workardoun for filenames being messed up
+output_mask_superfolder = sys.argv[-1]
 
-generate_mask(input_image_path, "test")
+#ensure input paths exist
+assert os.path.isdir(input_image_folder), f"{input_image_folder} is not a directory"
+assert os.path.isdir(output_mask_superfolder), f"{output_mask_superfolder} is not a directory"
+
+#wipe outputs folder
+base_directory_name = os.path.basename(input_image_folder)
+output_mask_folder = os.path.join(output_mask_superfolder, base_directory_name)
+if os.path.isdir(output_mask_folder): shutil.rmtree(output_mask_folder, ignore_errors=True)
+os.mkdir(output_mask_folder)     
+
+function_prototype = functools.partial(generate_mask, output_mask_folder=output_mask_folder)
+
+file_names = [os.path.join(input_image_folder, file) for file in os.listdir(input_image_folder)]
+
+file_series = pd.Series(file_names)
+
+file_series.p_map(function_prototype)
+print(f"Finished {base_directory_name}")
     
--- workflow/generate_masks/generate_masks.sh
+++ workflow/generate_masks/generate_masks.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+#SBATCH -N 3
+#SBATCH -n 2
 
 # ACTIVATE ANACONDA
 eval "$(conda shell.bash hook)"
 conda activate plantenv_pcv4_jupyter
 
-python3 generate_masks.py
+input_dataset1_folder="/mnt/stor/ceph/csb/marsfarm/projects/marsfarm_image_analysis/inputs/MV1-0039_7.11.23-8.11.23(Tomato)"
+input_dataset2_folder="/mnt/stor/ceph/csb/marsfarm/projects/marsfarm_image_analysis/inputs/MV1-0041_6.2.23-6.20.23(Peas)"
+input_dataset3_folder="/mnt/stor/ceph/csb/marsfarm/projects/marsfarm_image_analysis/inputs/MV1-0043_6.1.23-6.16.23(Purple Basil)"
+
+run_script()
+{
+    local input_folder=$1
+    local output_folder=/mnt/stor/ceph/csb/marsfarm/projects/marsfarm_image_analysis/inputs/masks
+    srun --time=1-00:00:00 --nodes=1 --ntasks-per-node=1 --exclusive -p general --job-name=jupyterlab python3 generate_masks.py "$input_folder" $output_folder &
+}
+
+run_script "$input_dataset1_folder"
+run_script "$input_dataset2_folder"
+run_script "$input_dataset3_folder"
+wait 
