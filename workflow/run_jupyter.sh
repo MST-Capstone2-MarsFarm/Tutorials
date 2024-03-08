ba571b387d7b3e5ce9328c74aae117dc2acc7c73
ajc3xc <ajc3xc@umsystem.edu>
2024-03-08 06:02:24 -0600

    updated sh script to not show output when running jupyter notebook.

--- workflow/run_jupyter.sh
+++ workflow/run_jupyter.sh
@@ -4,7 +4,7 @@
 eval "$(conda shell.bash hook)"
 conda activate plantenv_pcv4_jupyter
 
-srun --time=1-00:00:00 -p general --job-name=jupyterlab jupyter lab --no-browser --port=8888 &
+srun --time=1-00:00:00 -p general --job-name=jupyterlab jupyter lab --no-browser --port=8888 > /dev/null 2>&1 &
 #get node name that you most recently submitted
 compute_node_name="$(squeue --noheader -u $(whoami) --format=%N | tail -n 1)"
 sleep 5
