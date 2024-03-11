6cc0459b136b900ae64f9bb8d126a7a92e6f11fa
ajc3xc <ajc3xc@umsystem.edu>
2024-03-11 07:34:36 -0500

    continuing to test different parallelization regimes

--- workflow/full_run/full_run.py
+++ workflow/full_run/full_run.py
@@ -1,8 +1,15 @@
 import pandas as pd
-#import plantcv as pcv
-#import swifter
+import numpy as np
+from plantcv import plantcv as pcv
+import cv2
+from pandarallel import pandarallel
+pandarallel.initialize(progress_bar=True)
+
+from swifter import set_defaults
 from pathlib import Path, PosixPath
+from shutil import copy
 import os, sys
+from time import time
 
 input_folders = [
     Path("/mnt/stor/ceph/csb/marsfarm/projects/inputs/MV1-0039_7.11.23-8.11.23(Tomato)"),
@@ -23,11 +30,44 @@ def create_output_file(input_file_path: PosixPath):
     return str(output_path)
 file_output_paths = [create_output_file(input_file_path) for input_file_path in full_input_paths]
 input_str_file_paths = tuple(str(input_path) for input_path in full_input_paths)
-print(file_output_paths)
-#parameters_df = pd.DataFrame({"Input_Path": input_str_file_paths, "Output_Path": file_output_paths})
-#print(parameters_df)
+#print(file_output_paths)
+parameters_df = pd.DataFrame({"Input_Path": input_str_file_paths, "Output_Path": file_output_paths})
 
-#testing for one small dataset
-#df['input_path'] = None
+def filter_images(input_path: str, output_path: str):
+    #plantcv's readimage uses opencv's imread as a backend
+    image, _, _ = pcv.readimage(input_path)
+    
+    # Convert the image from RGB to HSV
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    
+    # Define the range of green color in HSV
+    # Adjusted range of green color in HSV to reduce false positives
+    lower_green = np.array([25, 40, 40])
+    upper_green = np.array([95, 255, 255])
+    
+    # Threshold the HSV image to get only green colors
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    
+    # Bitwise-AND mask and original image to extract green parts
+    plant_pixels = cv2.bitwise_and(image, image, mask=mask)
 
+    #convert to grayscale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Count all non-zero (non-black) pixels
+    count_non_black = cv2.countNonZero(gray_image)
+    
+    #using 20k pixels as a lowball starting point
+    #trying to make sure we get as many plant photos as possible
+    #using shutil.copy to make things quicker
+    if count_non_black > 20000:
+        copy(input_path, output_path)
+
+#pandarallel apply
+#pandarallel took 25 seconds on 1 general node
+#27.1345 seconds on head node
+
+start_time = time()
+parameters_df.parallel_apply(lambda row: filter_images(row['Input_Path'], row['Output_Path']), axis=1)
+print(time() - start_time)
 #df['input'].swifter.apply(my_func)
--- workflow/full_run/full_run.sh
+++ workflow/full_run/full_run.sh
@@ -4,4 +4,5 @@
 eval "$(conda shell.bash hook)"
 conda activate plantenv_pcv4_jupyter
 
-python3 full_run.py
+NUM_CPUS=$(sinfo -e -N -h -O cpus | head -n 1 | awk '{print $1}')
+srun --time=1-00:00:00 --cpus-per-task=$NUM_CPUS --tasks-per-node=1 -p general --job-name=leaf_detection python3 full_run.py
